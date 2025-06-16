fd2ecb04702381bcc964ecf91cc099506584dcec
Noel Miller <noel@noelmiller.dev>
2025-06-16 12:08:55 -0500

    disable services for now, maybe there is a way to do it

--- scripts/enable_services.sh
+++ scripts/enable_services.sh
@@ -3,6 +3,6 @@
 set -ouex pipefail
 
 systemctl enable docker.socket
-systemctl add-wants niri.service mako.service
-systemctl add-wants niri.service waybar.service
-systemctl add-wants niri.service swaybg.service
+# systemctl add-wants niri.service mako.service
+# systemctl add-wants niri.service waybar.service
+# systemctl add-wants niri.service swaybg.service
